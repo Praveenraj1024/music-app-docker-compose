3fe6569222e2cabe872a33e54c8aeba5e59ca99b
praveen <praveenrajjoth@gmail.com>
2019-08-11 19:07:11 +0530

    music-app is created and docker-composing is also completed.

--- mongo-track-service/src/main/java/com/stackroute/TrackServiceApplication.java
+++ mongo-track-service/src/main/java/com/stackroute/TrackServiceApplication.java
@@ -2,6 +2,7 @@ package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.PropertySource;
 //Used to specify the starting point of the project.
 //It triggers the auto-configuration, component scanning, bean creations.
 @PropertySource("classpath:application.properties")
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class TrackServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/stackroute/UserserviceApplication.java
+++ user-service/src/main/java/com/stackroute/UserserviceApplication.java
@@ -2,12 +2,13 @@ package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //Used to specify the starting point of the project.
 //It triggers the auto-configuration, component scanning, bean creations.
-@EnableEurekaClient
+//@EnableDiscoveryClient
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
